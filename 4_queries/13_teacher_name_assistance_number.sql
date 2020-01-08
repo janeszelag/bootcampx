585d4db5ab1777cd858bc2b015bc42314797739f
Katherine <katherine.szelag@mail.utoronto.ca>
2020-01-08 01:21:09 +0000

    sub selects

---
+++ 4_queries/13_teacher_name_assistance_number.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teachers_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teacher.name, cohort.name 
+ORDER BY teacher; 
